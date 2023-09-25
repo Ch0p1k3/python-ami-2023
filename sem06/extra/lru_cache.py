29019620fbd6ad0303d1ba471d61374996c0f15b
Ch0p1k3 <ch00p.228@gmail.com>
2023-09-26 00:40:17 +0300

    sem06 extra decorators

--- sem06/extra/lru_cache.py
+++ sem06/extra/lru_cache.py
@@ -3,7 +3,7 @@ import functools
 
 def cache(max_size):
     def wrapper(func):
-        if max_size == 0:
+        if max_size == 0:  # здесь мы не реализуем кеш, так как в аргументе нам задали, что кеш пустой
             @functools.wraps(func)
             def first_wraps(*args, **kwargs):
                 return func(args, kwargs)
@@ -14,21 +14,34 @@ def cache(max_size):
 
         @functools.wraps(func)
         def second_wraps(*args, **kwargs):
+            # Идея не сложная. Для хранения будем использовать словарик.
+            # Ключом у нас будет хэш от аргументов, но здесь нужно аккуратно:
+            # - сначала храним позиционные аргументы
+            # - разделяем их "dummy" (https://www.webopedia.com/definitions/dummy/) объектом
+            # - после храним именованные аргументы в виде кортежа (ключ, значение)
             key = args + (obj, )
             for item in kwargs.items():
                 key += item
 
+            # Пользуемся встроенной хеш-функцией.
+            # Кажется, что это необязательно и можно обойтись одним кортежом аргументов,
+            # но у меня такое решение :)
             hash_key = hash(key)
 
+            # Проверяем на то, если у нас этот подсчитанный хеш (то есть вызывали ли мы уже функцию от такой пачки аргументов).
+            # В случае успеха, достаем хеш и перекладываем его в конец (напомню, что встроенный dict по-умолчанию является ordered
+            # https://stackoverflow.com/questions/39980323/are-dictionaries-ordered-in-python-3-6)
             if hash_key in used:
                 result = used[hash_key]
                 del used[hash_key]
                 used[hash_key] = result
                 return result
 
+            # В инном случае освобождаем наш словарик, если он полон
             if len(used) == max_size:
                 _ = used.popitem(last=False)
 
+            # Подсчитываем функцию и записываем в наш словарик
             result = func(*args, **kwargs)
             used[hash_key] = result
 
